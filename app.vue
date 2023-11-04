b9add3049c40ed22b43dc37529128df5cd315bba
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2023-11-04 15:14:01 +0500

    added swiper module on the index page

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
 <template>
   <NuxtLayout> <NuxtPage /></NuxtLayout>
 </template>
+
+<script setup>
+import "swiper/css";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,50 @@
 <template>
   <div
-    class="bg max-w-full h-screen linear-gradient(rgb(255, 228, 230), rgb(204, 251, 241))"
+    class="max-w-full bg-[linear-gradient(rgb(255,_228,_230),_rgb(204,_251,_241))]"
   >
     <div class="container">
       <section class="section">
-        <div class="intro">
-          <h3 class="text-sm">BEST ONLINE STORE OF THE YEAR</h3>
-          <h1 class="text-lg">We don't do fashion, we are fashion</h1>
-          <button>Shop Now</button>
+        <div
+          class="intro flex flex-col text-center justify-center align-center py-10 space-y-4"
+        >
+          <h3 class="text-2xl text-gray-700 font-semibold">
+            BEST ONLINE STORE OF THE YEAR
+          </h3>
+          <h1 class="text-3xl font-bold text-gray-700">
+            We don't do fashion, we are fashion
+          </h1>
+          <button
+            class="font-medium bg-blue-800 w-max mx-auto p-2 rounded-lg text-white"
+            @click="$router.push('/products')"
+          >
+            Shop Now
+          </button>
         </div>
-        
+
+        <Swiper
+          :slides-per-view="3"
+          :space-between="50"
+          :autoplay="{ delay: 2_500, pauseOnMouseEnter: true }"
+          :speed="1_000"
+          :loop="true"
+          :navigation="true"
+          :modules="[Navigation, Autoplay]"
+        >
+          <SwiperSlide v-for="i of 9" :key="i">
+            <Card />
+          </SwiperSlide>
+        </Swiper>
       </section>
     </div>
   </div>
 </template>
-<script setup></script>
+
+<script setup>
+import { Swiper, SwiperSlide } from "swiper/vue";
+import { Navigation, Autoplay } from "swiper/modules";
+
+// Css
+import "swiper/css/navigation";
+</script>
+
 <style lang=""></style>
