9b49f22c0abd0aff717f26c71eca17a673586a5e
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2023-11-04 13:06:35 +0500

    added shop text on the cart page

--- pages/cart.vue
+++ pages/cart.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="container"><h1>Cart</h1></div>
+  <div class="container">
+    <h1>Cart</h1>
+    <p>Shop</p>
+  </div>
 </template>
 <script setup></script>
 <style lang=""></style>
