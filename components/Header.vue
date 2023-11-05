9dda9b3f6f1672e5766a1a555170c52d357a5418
nureekdevell <58046604+nureekdevell@users.noreply.github.com>
2023-11-05 12:48:53 +0500

    added cart logo//

--- components/Header.vue
+++ components/Header.vue
@@ -21,7 +21,7 @@
           <NuxtLink
             href="/cart"
             type="button"
-            class="relative text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            class="flex items-center relative text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             <div
               v-if="count > 0"
@@ -29,6 +29,7 @@
             >
               {{ count }}
             </div>
+            <img class="w-6 h-6 mr-1" src="/cart.svg" alt="" />
             Cart
           </NuxtLink>
           <NuxtLink
